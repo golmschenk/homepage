6afcb18698e9adb915f50f1e1098b7690d4c2f3e
Greg Olmschenk <golmschenk@gmail.com>
2015-12-29 23:30:13 -0600

    FunctionalTests: Add base functional test

---
+++ functional_tests/base.py
@@ -0,0 +1,14 @@
+"""
+Functional template base class.
+"""
+from selenium import webdriver
+from django.contrib.staticfiles.testing import StaticLiveServerTestCase
+
+
+class BaseFunctionalTest(StaticLiveServerTestCase):
+
+    def setUp(self):
+        self.browser = webdriver.Firefox()
+
+    def tearDown(self):
+        self.browser.quit()
