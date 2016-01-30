e602d4fa58a0d401753306a9953dcb7b1c980db9
Greg Olmschenk <golmschenk@gmail.com>
2016-01-30 16:13:18 -0500

    Register Essay and EducationEntry with admin

--- profile/admin.py
+++ profile/admin.py
@@ -1,4 +1,9 @@
 """
 The homepage project admin registration.
 """
-# Register your models here.
+from django.contrib import admin
+
+from profile.models import EducationEntry, Essay
+
+admin.site.register(EducationEntry)
+admin.site.register(Essay)
