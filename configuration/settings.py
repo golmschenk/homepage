d749fd15cb175d1e8e91482909697d316c505992
Greg Olmschenk <golmschenk@gmail.com>
2016-02-05 18:19:14 -0500

    Add teaching element to the homepage

--- configuration/settings.py
+++ configuration/settings.py
@@ -50,6 +50,7 @@ MIDDLEWARE_CLASSES = (
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
     'django.middleware.security.SecurityMiddleware',
+    'configuration.middleware.prettify.PrettifyMiddleware'
 )
 
 ROOT_URLCONF = 'configuration.urls'
