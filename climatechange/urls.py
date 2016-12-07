ececff6dea9cbf5229ebcef5a87224c8932d02e8
Greg Olmschenk <golmschenk@gmail.com>
2016-12-07 15:09:06 -0500

    Fix overlapping url name bug

--- climatechange/urls.py
+++ climatechange/urls.py
@@ -6,5 +6,5 @@ from django.conf.urls import url
 from climatechange.views import HomeView
 
 urlpatterns = [
-    url(r'^$', HomeView.as_view(), name='home'),
+    url(r'^$', HomeView.as_view(), name='climatechange'),
 ]
